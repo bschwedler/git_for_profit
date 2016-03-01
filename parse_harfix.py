37db802b4505476362a99fc29c3eb23a3aeff723
Benjamin R. J. Schwedler <Benjamin.Schwedler@noaa.gov>
2016-03-01 15:44:35 -0600

    Loop through each line of HARFIX.txt file
    $ git commit -a

--- parse_harfix.py
+++ parse_harfix.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import sys
+
+filename = 'HARFIX.txt'
+print filename
+
+f = open(filename)
+for line in f:
+    # Line varible includes newline at the end
+    sys.stdout.write(line)
+
+f.close()
+
