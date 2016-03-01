ab5f331a9815cd98b3cff0ee15d4debf0475c4b8
Benjamin R. J. Schwedler <Benjamin.Schwedler@noaa.gov>
2016-03-01 15:52:56 -0600

    Parse each field using string index
    Parse fields in HARFIX.txt using string index.
    
    This method is very brute force, doesn't do anything with formatting,
    and is not very pythonic
    
    $ git diff
    $ git commit -a

--- parse_harfix.py
+++ parse_harfix.py
@@ -8,8 +8,16 @@ print filename
 
 f = open(filename)
 for line in f:
-    # Line varible includes newline at the end
-    sys.stdout.write(line)
+    point_id = line[0:77]
+    point_lon = line[78:90]
+    point_lat = line[91:104]
+    point_type = line[105:106]
+    point_class = line[107:118]
+    point_pitch = line[119:120]
+    point_catch = line[121:122]
+    point_way = line[123:124]
+    print point_id, point_lon, point_lat, point_type, point_class, \
+            point_pitch, point_catch, point_way
 
 f.close()
 
