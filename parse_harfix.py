8bd4840f6c58f18cab355a439ab42a2217dc3885
Benjamin R. J. Schwedler <Benjamin.Schwedler@noaa.gov>
2016-03-01 16:27:22 -0600

    Strip of extraneous characters from parsed input
    $ git commit -a

--- parse_harfix.py
+++ parse_harfix.py
@@ -8,16 +8,14 @@ print filename
 
 f = open(filename)
 for line in f:
-    point_id = line[0:77]
-    point_lon = line[78:90]
-    point_lat = line[91:104]
-    point_type = line[105:106]
-    point_class = line[107:118]
-    point_pitch = line[119:120]
-    point_catch = line[121:122]
-    point_way = line[123:124]
-    print point_id, point_lon, point_lat, point_type, point_class, \
-            point_pitch, point_catch, point_way
+    point_id = line[0:77].strip()
+    point_lon = line[78:90].strip()
+    point_lat = line[91:104].strip()
+    point_type = line[105:106].strip()
+    point_class = line[107:118].strip()
+    point_pitch = line[119:120].strip()
+    point_catch = line[121:122].strip()
+    point_way = line[123:124].strip()
 
 f.close()
 
